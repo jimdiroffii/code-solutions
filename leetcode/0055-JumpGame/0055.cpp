7e28b82c21f6e6142b3dfad5f6d181f3e86f3f5f
Jim Diroff II <jim@dirofftech.com>
2024-01-02 18:31:32 -0500

    Add LC 0055 in CPP

---
+++ leetcode/0055-JumpGame/0055.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution
+{
+public:
+	bool canJump(std::vector<int> &nums)
+	{
+		int lastPos = nums.size() - 1;
+		for (int i = nums.size() - 1; i >= 0; --i)
+		{
+			if (i + nums[i] >= lastPos)
+			{
+				lastPos = i;
+			}
+		}
+
+		return lastPos == 0;
+	}
+};
